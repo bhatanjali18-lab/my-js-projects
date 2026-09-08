e300dd71d6bbfe47860176bc9536599b5ea1de6d
anjali <bhatanjali18@gmail.com>
2026-09-08 19:33:04 +0530

    removed all files from the repo

--- js/s.js
+++
@@ -1,6 +0,0 @@
-let number=prompt("enter your number")
-number=Number.parseInt(number)
-
-if(number>4){
-  location.href="https://google.com"
-}
--- js/scripts.js
+++
@@ -1,3 +0,0 @@
-console.log("js is connected")
-let heading=document.querySelectorAll("h1");
-console.log(heading);
